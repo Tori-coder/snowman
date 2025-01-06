bc00acd64a0eb09434fffedd0602d5b908ef4033
Tori-coder <114353884+Tori-coder@users.noreply.github.com>
2025-01-06 11:51:27 +0000

    set up menus

--- run.py
+++ run.py
@@ -13,6 +13,44 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('snowman')
 
 scores = SHEET.worksheet('scores')
-data = scores.get_all_values()
 
-print(data)
+# Main Game
+
+def menu():
+    print("Main Menu:\n")
+    print("1 Instructions\n")
+    print("2 Play Game\n")
+
+    while True:
+        menu_option = input("Choose from menu: ")
+        if menu_option == "1":
+            return instructions()
+        elif menu_option == "2":
+            return play_game()
+        else:
+            print("Please choose 1 or 2")
+       
+
+
+def instructions():
+    print("instructions here")
+
+
+def play_game():
+    print("\n Difficulty level\n")
+    print("1 Easy\n")
+    print("2 Moderate\n")
+    print("3 Hard\n")
+
+    while True:
+        menu_option = input("Choose difficulty level: ")
+        if menu_option == "1":
+            print("easy")
+        elif menu_option == "2":
+            print("moderate")
+        elif menu_option == "3":
+            print("difficult")
+        else:
+            print("Please choose 1,2 or 3")
+
+menu()
